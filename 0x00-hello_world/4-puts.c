9c2183b05355db8ad9652d8ddb36dd0e1adbe2bc
hindeljadmioui <eljadmioui.hind@gmail.com>
2023-02-16 08:14:17 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programmig is like building a multilingual
+ * puzzle, followd by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"programing is like building a multilingaul puzzle");
+
+	return(0);
+}
